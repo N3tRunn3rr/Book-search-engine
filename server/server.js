581cb7aac385c1d47c479cfb748dd167d61622b7
Kenji Fleming <kwf1999@gmail.com>
2023-03-20 16:07:25 -0400

    added schema folder and added code to js files inside

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
+
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
+
+const path = require('path');
+const db = require('./config/connection');
+
 const { typeDefs, resolvers } = require('./schemas');
 const routes = require('./routes');
 
